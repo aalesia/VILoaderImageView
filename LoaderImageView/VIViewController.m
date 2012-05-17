d30287115a877e115d5384149dfb3fcb90ffd6e4
Anthony Alesia <aalesia88@gmail.com>
2012-05-17 15:53:07 -0500

    added grid for your viewing pleasure

--- LoaderImageView/VIViewController.m
+++ LoaderImageView/VIViewController.m
@@ -19,10 +19,80 @@
 {
     [super viewDidLoad];
     
-    VILoaderImageView *imageView = [[VILoaderImageView alloc] initWithFrame:CGRectMake(100, 100, 250, 250)
+    VILoaderImageView *imageView = [[VILoaderImageView alloc] initWithFrame:CGRectMake(12, 84, 200, 200)
                                                                    imageUrl:@"http://media.threadless.com//imgs/products/2943/636x460design_01.jpg" 
                                                                    animated:YES];
     [self.view addSubview:imageView];
+    
+    VILoaderImageView *imageView1 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(212, 84, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/3770/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView1];
+    
+    VILoaderImageView *imageView2 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(412, 84, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/3634/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView2];
+    
+    VILoaderImageView *imageView3 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(612, 84, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/3468/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView3];
+    
+    VILoaderImageView *imageView4 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(812, 84, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/3124/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView4];
+    
+    VILoaderImageView *imageView5 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(12, 284, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/3111/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView5];
+    
+    VILoaderImageView *imageView6 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(212, 284, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/2945/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView6];
+    
+    VILoaderImageView *imageView7 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(412, 284, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/2894/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView7];
+    
+    VILoaderImageView *imageView8 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(612, 284, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/2602/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView8];
+    
+    VILoaderImageView *imageView9 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(812, 284, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/2585/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView9];
+    
+    VILoaderImageView *imageView10 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(12, 484, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/1731/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView10];
+    
+    VILoaderImageView *imageView11 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(212, 484, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/2300/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView11];
+    
+    VILoaderImageView *imageView12 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(412, 484, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/1951/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView12];
+    
+    VILoaderImageView *imageView13 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(612, 484, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/1944/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView13];
+    
+    VILoaderImageView *imageView14 = [[VILoaderImageView alloc] initWithFrame:CGRectMake(812, 484, 200, 200)
+                                                                   imageUrl:@"http://media.threadless.com//imgs/products/3773/636x460design_01.jpg" 
+                                                                   animated:YES];
+    [self.view addSubview:imageView14];
 }
 
 - (void)viewDidUnload
@@ -33,7 +103,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return YES;
+    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
 
 @end
