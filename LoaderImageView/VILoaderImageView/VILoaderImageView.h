ded42272c39972a85cb62e10892c6e2a8a8f9c6b
teejay <tfallon@mail.depaul.edu>
2012-11-27 10:52:49 -0600

    Defaulting to Animated, couple fixes for animation, caching, and addition of Block Methods from the Threadless version of VILoaderImage

--- LoaderImageView/VILoaderImageView/VILoaderImageView.h
+++ LoaderImageView/VILoaderImageView/VILoaderImageView.h
@@ -14,20 +14,21 @@
 
 }
 
-@property (strong, nonatomic) UIColor *indicatorColor;
++ (NSOperationQueue *)getQueue;
++ (void)cacheImage:(NSString *)imageURLString completion:(void (^)(UIImage *image))completion;
 
 - (id)initWithFrame:(CGRect)frame imageUrl:(NSString *)imageUrl;
 - (id)initWithFrame:(CGRect)frame imageUrl:(NSString *)imageUrl animated:(BOOL)animated;
+
 - (void)setImageUrl:(NSString *)imageUrl;
 - (void)setImageUrl:(NSString *)imageUrl animated:(BOOL)animated;
-- (void)setImageUrl:(NSString *)imageUrl defaultImage:(UIImage *)defaultImage;
-- (void)setImageUrl:(NSString *)imageUrl defaultImage:(UIImage *)defaultImage animated:(BOOL)animated;
 - (void)setImageUrlClearingCache:(NSString *)imageUrl animated:(BOOL)animated;
+- (void)setImageUrl:(NSString *)imageUrl completion:(void (^)(UIImage *image))completion;
 
 - (void)startIndicatorAnimating;
 - (void)stopIndicatorAnimating;
 
 - (void)cancel;
 
-+ (void)cacheImage:(NSString *)imageURLString completion:(void (^)(UIImage *image))completion;
+
 @end
